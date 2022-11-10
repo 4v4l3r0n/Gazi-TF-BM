72ffda25757d9122a9c850ed751297b1e15f71f3
4v4l3r0n <busegulgunay@gmail.com>
2022-11-10 14:58:50 +0300

    .

--- yildiz_piramidi.c
+++ yildiz_piramidi.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-int main() {
-   int a, bosluk, satir, b = 0;
-   printf("Satır sayısını giriniz: ");
-   scanf("%d", &satir);
-   for (a = 1; a <= satir; ++a, b = 0) {
-      for (bosluk = 1; bosluk <= satir - a; ++bosluk) {
-         printf("  ");
-      }
-      while (b != 2 * a - 1) {
-         printf("* ");
-         ++b;
-      }
-      printf("\n");
-   }
-   return 0;
+
+int main()
+{
+    int satir;
+    printf("Satir sayisini giriniz.\n");
+    scanf("%d",&satir);
+    for (int a=0;a<satir;a++){
+         
+         for(int b = a;satir-b>0;b++){
+            printf(" ");
+         }
+         for(int c=1;c<=(2*a+1);c++){
+            printf("*");
+         }
+        printf("\n");
+
+    }
+return 0;
 }
